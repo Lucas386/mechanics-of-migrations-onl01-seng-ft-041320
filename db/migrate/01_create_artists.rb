bba4678fd1e4daae2383f8c06943a22540bef1a9
Lucas Easley <lucaseasley11@gmail.com>
2020-05-16 16:13:28 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,31 +1,3 @@
-# db/migrate/01_create_artists.rb
-
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
-
-ActiveRecord::Base.establish_connection(
- :adapter => "sqlite3",
- :database => "db/artists.sqlite"
-)
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
-
-def change
-  create_table :artists do |t|
-  end
-end
-
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
